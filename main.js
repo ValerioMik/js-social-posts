6b2202ac0461c69cd34583bd304942d4e3a80613
ValerioMik <valerio.avallone@hotmail.com>
2021-11-05 17:07:35 +0100

    Esercizio quasi completato mi manca il click

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ let inFormazioniPost = [
         "imgAutore":"https://unsplash.it/300/300?image=15",
         "nomeAutore":"Valerio Avallone",
         "dataPost":"2 mesi fa",
-        "testoPost":">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias",
+        "testoPost":"Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias",
         "imgPost":"https://unsplash.it/600/300?image=171",
         "numerMipiace":"80"
     },
@@ -33,15 +33,14 @@ let inFormazioniPost = [
         "imgAutore":"https://unsplash.it/300/300?image=15",
         "nomeAutore":"Giuseppe Vivaldi",
         "dataPost":"6 mesi fa",
-        "testoPost":">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias",
-        "imgPost":"https://unsplash.it/600/300?image=171",
+        "testoPost":"Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias",
         "numerMipiace":"50"
     },
     {
         "imgAutore":"https://unsplash.it/300/300?image=15",
         "nomeAutore":"Giorgione Antico",
         "dataPost":"1 mesi fa",
-        "testoPost":">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias",
+        "testoPost":"Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias",
         "imgPost":"https://unsplash.it/600/300?image=171",
         "numerMipiace":"30"
     },
@@ -49,30 +48,41 @@ let inFormazioniPost = [
         "imgAutore":"https://unsplash.it/300/300?image=15",
         "nomeAutore":"Antonio Paperella",
         "dataPost":" 1 ora fa",
-        "testoPost":">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias",
-        "imgPost":"https://unsplash.it/600/300?image=171",
+        "testoPost":"Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias",
         "numerMipiace":"13"
     },
     {
         "imgAutore":"https://unsplash.it/300/300?image=15",
         "nomeAutore":"Papis Peppino",
         "dataPost":"4 ore fa",
-        "testoPost":">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias",
+        "testoPost":"Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias",
         "imgPost":"https://unsplash.it/600/300?image=171",
         "numerMipiace":"180"
     },
-],
+    {
+        "imgAutore":"https://unsplash.it/300/300?image=15",
+        "nomeAutore":"Valerio Avallone",
+        "dataPost":"2 mesi fa",
+        "testoPost":"Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias",
+        "imgPost":"https://unsplash.it/600/300?image=171",
+        "numerMipiace":"80"
+    },
+];
+console.log(inFormazioniPost);
 
-const postCont = document.getElementsByClassName("post");
 
-for (let i = 0;inFormazioniPost.length; i++ ){
-    const{imgAutore,nomeAutore,dataPost,testoPost,imgPost,numerMipiace} = inFormazioniPost[i];
+
+const postCont = document.querySelector('.post');
+
+for (let i = 0; i < inFormazioniPost.length; i++ ){
+    const {imgAutore,nomeAutore,dataPost,testoPost,imgPost,numerMipiace} = inFormazioniPost[i];
     console.log(inFormazioniPost);
-    postCont.innerHTML +=`
-            <div class="post__header">
+    if(imgPost!== undefined){
+    postCont.innerHTML += `
+        <div class="post__header">
             <div class="post-meta">                    
                 <div class="post-meta__icon">
-                    <img class="profile-pic" src=${imgAutore} alt="Phil Mangione">                    
+                    <img class="profile-pic" src= ${imgAutore} alt="Phil Mangione">                    
                 </div>
                 <div class="post-meta__data">
                     <div class="post-meta__author">${nomeAutore}</div>
@@ -80,9 +90,10 @@ for (let i = 0;inFormazioniPost.length; i++ ){
                 </div>                    
             </div>
         </div>
-        <div class="post__text">${testoPost}
-            <img src=${imgPost} alt="">
-        </div>
+            <div class="post__text">${testoPost}</div>
+            <div class="post__image">
+                <img src= ${imgPost} alt="">
+            </div>
         <div class="post__footer">
             <div class="likes js-likes">
                 <div class="likes__cta">
@@ -96,8 +107,50 @@ for (let i = 0;inFormazioniPost.length; i++ ){
                 </div>
             </div> 
         </div>    
-    `
+        `}else {
+        postCont.innerHTML +=  `
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src= ${imgAutore} alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${nomeAutore}</div>
+                    <div class="post-meta__time">${dataPost}</div>
+                </div>                    
+            </div>
+        </div>
+            <div class="post__text">${testoPost}</div>
+            <div class="post__image">
+            </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${numerMipiace}</b> persone
+                </div>
+            </div> 
+        </div>    
+        `}
+        
 };
 
 
-    
+const{numerMipiace} = inFormazioniPost[i];
+
+const botTon= querySelectorAll("like-button");
+
+for(let i = 0 ; i < botTon.length;i++){
+    botTon.addEventListener ("click",
+    function(){
+        numerMipiace++;
+    })
+   
+};
+
+
